7225ad252cfe682feaacd562a47f5233d41a236b
bingjiezi <alexzi13703@gmail.com>
2021-10-13 22:35:35 -0700

    Q7 push

---
+++ String/addString/src/edu/northeastern/bingjie/Main.java
@@ -0,0 +1,14 @@
+package edu.northeastern.bingjie;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution sl = new Solution();
+        String num1 = "11", num2 = "123";
+
+        String num11 = "456", num22 = "77";
+
+        System.out.println(sl.addStrings(num1,num2)); //134
+        System.out.println(sl.addStrings(num11,num22)); //533
+    }
+}
